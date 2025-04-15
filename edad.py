d7861d232c6c3e921064af5c95f2300b4b53614d
Gajardo-hub <vicente.gajardo.calderon@gmail.com>
2025-04-15 13:26:22 -0400

    Primera clase

--- edad.py
+++ edad.py
@@ -1,10 +1,29 @@
+# # edad=int(input("Ingrese su edad "))
 
-num=int(input("Ingrese su edad "))
+# # if edad<12:
+# #     print("tiene ", edad, "por tanto es un niño")
+# # elif edad<=12 and edad<=17:
+# #     print("tiene", edad, "por tanto es adolecente")
+# # else:
+# #     print("tiene", edad, "por tanto es mayor de edad")
 
-print("Su edad es", num)
+# # Ingrese 3 numeros y muestre el mayor de ellos
 
-if num>=18:
-    print("Usted es mayor de edad")
+# n1=int(input("ingrese un numero"))
+# n2=int(input("ingrese un numero"))
+# n3=int(input("ingrese un numero"))
+
+# if n1>n2 and n1>n3:
+#      print("El numero", n1, "es el mayor")
+# elif n2>n3:
+#      print("El numero", n2, "es el mayor")
+# else:
+#      print("El numero", n3, "es el mayor")
+
+# Par o Impar
+n1=int(input("ingrese un numero "))
+
+if n1 % 2==0:
+    print("El numero es par")
 else:
-    print("Usted es menor de edad")
-    
+    print("El numero es impar")

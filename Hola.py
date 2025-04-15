c0deaf0a0897d8eb5b1f9709c9324bc40972584c
Gajardo-hub <vicente.gajardo.calderon@gmail.com>
2025-04-15 11:54:18 -0400

    Primer commit

---
+++ Hola.py
@@ -0,0 +1,16 @@
+# # #declaracion de variables
+# # nombre="Finn"
+# # edad=23
+# # #ejemplo de concatenacion
+# # print("Hola", nombre,"tu edad es",edad)
+
+# print("ingrese su nombre")
+# nombre=input
+
+# print("Hola", nombre,"tu edad es",edad)
+
+print("ingrese 2 numeros")
+
+n1=int(input())
+n2=int(input())
+print("El resultado de la suma es", n1+n2) 

725196e85b4cad3f96cf0ad43fb5759308e77568
Gajardo-Hub <vicente.gajardo.calderon@gmail.cl>
2025-05-29 13:31:50 -0400

    lista

---
+++ listas.py
@@ -0,0 +1,57 @@
+# uso y explicacion de LISTAS
+
+#     -6 -5 -4 -3  -2 -1
+# lista=[5, 7, 2, 9, 10, 2]
+# #      0  1  2  3  4   5
+ 
+# print(lista[-6]) # acceso al valor por indice nevativo
+# print(lista[0]) # acceso al valor por indice positivo
+
+# # mostrar toda la lista
+# print(lista)
+
+# for num in lista:
+#     print(num)
+
+# hacer una lista de 5 notas luego sacar su promedio. Las notas deben ser valores int ej: 55,32.
+
+# notas=[34,45,65,70]
+# print(notas)
+# c=0
+# suma=0
+# for nota in notas:
+#    suma+=nota
+#    c+=1
+# prom=suma/c
+# print("El promedio es ", round(prom))
+
+# for nota in notas:
+#    if notas>=40:
+#       print("Paso")
+#    else:
+#       print("NO PASO")
+      
+
+
+# nombres=["rei", "cony", "Kun"]
+# apellidos=["Hood", "Kai", "Chan"]
+
+# print(len(nombres))
+
+# for i in range(len(nombres)):
+#     print(f"Su nombre es {nombres[i]} {apellidos[i]}")
+
+# frutas=["melo", "sandia", "kiwi"]
+# for fruta in frutas:
+#     print (f"La {fruta} tiene {len(fruta)} caracteres")
+
+# autos=["audi", "toyota", "BMW", "KIA", "Mercedes"]
+
+# for auto in autos:
+#     print(auto)
+#     if "a" in auto.lower():
+#         print("La marca contiene una A")
+#     else:
+#         print("No contiene A")
+
+  

52ad876a081777035a78b5900b0cd18ca2314adc
Gajardo-hub <vicente.gajardo.calderon@gmail.com>
2025-06-10 13:28:33 -0400

    listas

--- listas.py
+++ listas.py
@@ -55,3 +55,48 @@
 #         print("No contiene A")
 
   
+
+# nombres=[]
+# apellidos=[]
+
+# while True:
+#     print('''
+#           1.- Insertar nombre y apellido
+#           2.- Buscar nombre
+#           3.- Buscar apellido
+#           4.- Mostrar nombres y apellidos
+#           5. Salir''')
+#     op=int(input("Seleccione una opcion: "))
+#     match op:
+#         case 1:
+#             nom=input("Ingrese un nombre: ")
+#             nombres.append(nom)
+#             print(nombres)
+#             ape=input("Ingrese un apellido: ")
+#             apellidos.append(ape)
+#             print(apellidos)
+#         case 2:
+#             buscar=input("Busque un nombre: ")
+#             if buscar in nombres:
+#                 print(f"El nombre {buscar} SI encuentra en la lista")
+#             else:
+#                 print(f"El nombre {buscar} NO se encuentra en la lista")
+#         case 3:
+#             buscar=input("Busque un apellido: ")
+#             if buscar in apellidos:
+#                 print(f"El apellido {buscar} SI encuentra en la lista")
+#             else:
+#                 print(f"El apellido {buscar} NO se encuentra en la lista")
+#         case 4:
+#             cont=0
+#             for i in nombres:
+#                 print(cont+1,".-", nombres[cont], apellidos[cont])
+#                 cont+=1
+#         case 5:
+#             print("Saliendo...")
+#             break
+#         case _:
+#             print("Selecciona una opcion valida")
+        
+        
+        
